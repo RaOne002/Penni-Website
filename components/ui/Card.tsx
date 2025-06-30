6c2578829b89501176db70076e26def171c10a51
Subhranil Mondal <subhranilmondal2@gmail.com>
2025-06-30 15:17:21 +0530

    Fix card name

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -109,7 +109,7 @@ export default function CardRow() {
                         <div className="flex flex-col justify-end h-full">
                             {(isMobile || isOpen) && (
                                 <button
-                                    className="flex items-center justify-between bg-mywhite text-myneutral-900 rounded-full px-6 py-5 m-4 text-2xl font-medium shadow transition hover:bg-myneutral-475"
+                                    className="flex items-center justify-between bg-mywhite text-myneutral-900 rounded-full px-6 py-5 m-4 text-xl font-medium shadow transition hover:bg-myneutral-475"
                                     style={{ minHeight: 48 }}
                                 >
                                     <span>{card.buttonText}</span>
