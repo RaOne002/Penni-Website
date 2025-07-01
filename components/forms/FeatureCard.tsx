0f6050d6c5e477f8663b6434ecb4322c270fd54e
Subhranil Mondal <subhranilmondal2@gmail.com>
2025-07-01 10:39:21 +0530

    added FetureCard also

--- components/forms/FeatureCard.tsx
+++ components/forms/FeatureCard.tsx
@@ -5,13 +5,13 @@ interface FeatureCardProps {
   description: string;
 }
 
-export const FeatureCard: React.FC<FeatureCardProps> = ({ title, description }) => {
+export const FeatureCard: React.FC<FeatureCardProps & { className?: string }> = ({ title, description, className }) => {
   return (
-    <article className="flex flex-col items-start py-7 pr-20 pl-6 mt-4 rounded-xl bg-neutral-700 max-md:px-5">
-      <h3 className="text-3xl font-bold text-zinc-400">
+    <article className={`flex flex-col items-start py-3 px-7 mt-2 rounded-xl bg-myneutral-700 max-md:px-5 ${className || ""}`}>
+      <h3 className="text-3xl font-bold text-myzinc-200">
         {title}
       </h3>
-      <p className="mt-4 text-2xl font-normal text-neutral-400">
+      <p className="mt-1 text-xl font-normal text-myzinc-300">
         {description}
       </p>
     </article>
